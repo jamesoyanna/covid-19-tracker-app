d2f2430fe805b44bd0b7cad856e8b1b4c49218c6
eshoiza <engrjayt200@gmail.com>
2020-04-25 23:11:44 +0100

    added styling to components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import {Cards, Chart, CountryPicker} from "./components";
-
+import styles from "./App.module.css"
 class App extends Component {
     render() {
-        return (
-          <div>
+        return ( 
+          <div className={styles.container}>
             <h1>I am App</h1>
             <Cards />
             <Chart />
