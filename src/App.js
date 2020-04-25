5fa114f82eb5c3a04c20fcef6548f4425d2c563d
eshoiza <engrjayt200@gmail.com>
2020-04-25 22:52:23 +0100

    created and added application components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+// import Cards from './components/Cards/Cards';
+// import Chart from "./components/Chart/Chart";
+// import CountryPicker from "./components/CountryPicker/CountryPicker";
+
+import {Cards, Charts, CountryPicker}
 
 class App extends Component {
     render() {
         return (
-            <div>
-               <h1>I am App</h1> 
-            </div>
-        )
+          <div>
+            <h1>I am App</h1>
+            <Cards />
+            <Chart />
+            <CountryPicker />
+          </div>
+        );
     }
 }
 export default App;
