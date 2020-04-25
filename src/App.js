8f764ea5d3e08c2f1431702d6304236e1b186966
eshoiza <engrjayt200@gmail.com>
2020-04-25 23:00:23 +0100

    updated compenents using name import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-// import Cards from './components/Cards/Cards';
-// import Chart from "./components/Chart/Chart";
-// import CountryPicker from "./components/CountryPicker/CountryPicker";
-
-import {Cards, Charts, CountryPicker}
+import {Cards, Chart, CountryPicker} from "./components";
 
 class App extends Component {
     render() {
