c1c46f291fda89f74bf9868c0d77fd305d55cf75
eshoiza <engrjayt200@gmail.com>
2020-04-26 18:43:37 +0100

    added a state a App component

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import styles from "./App.module.css";
 import { fetchData } from "./api";
 
 class App extends Component {
-    
+   
+    state = {
+        data: {},
+    }
     async componentDidMount(){
-        const data = await fetchData();
-
-        console.log(data);
+        const fetchData = await fetchData();
+        this.setState({data: fetchData})
     }
 
     render() {
+        const {data} = this.state;
         return ( 
           <div className={styles.container}>
             <h1>I am App</h1>
-            <Cards />
+            <Cards data = {data} />
             <Chart />
             <CountryPicker />
           </div>
