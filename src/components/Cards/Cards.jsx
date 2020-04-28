053a43742ca19d0ea0a79807fe19f152391096d3
eshoiza <engrjayt200@gmail.com>
2020-04-28 11:39:39 +0100

    imported classname for styling

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import {Card, CardContent, Typography, Grid} from "@material-ui/core";
 import CountUp from 'react-countup';
+import cx from 'classnames';
 import styles from './Cards.module.css'; 
 
 const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}})=>{
     if(!confirmed){
         return 'loading...'
     }
-    console.log(confirmed);
     return(
        <div className={styles.container}>
            <Grid container spacing={3} justify="center">
-             <Grid item component={Card}>
+             <Grid item component={Card} xs ={12} md={3} className={cx(styles.card, styles.infected)}>
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Infected</Typography>
                      <Typography variant="h5">
@@ -25,7 +25,7 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}})=>{
                      <Typography variant="body2">Number of active cases of covid-19</Typography>
                  </CardContent>
                  </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
                         <Typography variant="h5"><CountUp
@@ -38,7 +38,7 @@ const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}})=>{
                         <Typography variant="body2">Number of recovered cases from covid-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
                         <Typography variant="h5"><CountUp
