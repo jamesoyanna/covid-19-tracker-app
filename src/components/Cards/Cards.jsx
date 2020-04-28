c3491d9e464b63bb5c837c8449564507fbcbf2ce
eshoiza <engrjayt200@gmail.com>
2020-04-28 10:23:38 +0100

    added material ui design

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {Card, CardContent, Typography, Grid} from "@material-ui/core";
+import styles from './Cards.module.css'; 
 
 const Cards = (props)=>{
     console.log(props);
     return(
-        <h1>Cards</h1>
+       <div className={styles.container}>
+           <Grid container spacing={3} justify="center">
+             <Grid item component={Card}>
+                 <CardContent>
+                     <Typography color="textSecondary" gutterBotom="Infected"></Typography>
+                     <Typography variant="h5">REAL DATA</Typography>
+                     <Typography color="textSecondary">REAL DATE</Typography>
+                     <Typography variant="body2">Number of active cases of covid-19</Typography>
+                 </CardContent>
+                 </Grid>    
+           </Grid>
+       </div>
     )
 }
  export default Cards; 
