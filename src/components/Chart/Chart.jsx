053018c3efe4c12f3febde888826b3e6401d6657
eshoiza <engrjayt200@gmail.com>
2020-04-29 00:17:25 +0100

    added a line chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,19 +1,20 @@
 import React, {useState, useEffect} from 'react';
-import {fetchDailyData} from '../../api';
+import {fetchDailyData } from "../../api";
 import {Line, Bar} from 'react-chartjs-2';
 import styles from './Chart.module.css';
+
 const Chart = () => {
     const [dailyData, setDailyData] = useState([]);
+
     useEffect(()=>{
     const fetchAPI = async ()=>{
      setDailyData(await fetchDailyData());
     }
-    console.log(dailyData);
     fetchAPI();
     });
-
+     
     const LineChart = (
-        dailyData.length
+        dailyData.length 
          ?( 
         <Line 
         data={{
@@ -30,15 +31,14 @@ const Chart = () => {
                borderColor: 'red',
                backgroundColor:'rgba(255, 0,0,0.5)',
                fill: true,
-           }
-             ] 
+           }], 
         }}
       
-                />): null
+        />): null
     )
     return (
          <div className={styles.container}>
-        <LineChart />
+          <LineChart />
         </div>
     );
 }
