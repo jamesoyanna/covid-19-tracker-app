ff640492c215511a7c318f6097505342db37bb24
eshoiza <engrjayt200@gmail.com>
2020-04-28 10:49:12 +0100

    rendered number of confirmed cases on the ui

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import {Card, CardContent, Typography, Grid} from "@material-ui/core";
 import styles from './Cards.module.css'; 
 
-const Cards = (props)=>{
-    console.log(props);
+const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}})=>{
+    if(!confirmed){
+        return 'loading...'
+    }
+    console.log(confirmed);
     return(
        <div className={styles.container}>
            <Grid container spacing={3} justify="center">
              <Grid item component={Card}>
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                     <Typography variant="h5">REAL DATA</Typography>
+                     <Typography variant="h5">{confirmed.value}</Typography>
                      <Typography color="textSecondary">REAL DATE</Typography>
                      <Typography variant="body2">Number of active cases of covid-19</Typography>
                  </CardContent>
